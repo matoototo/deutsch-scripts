6f009f20a6164afcc0d5573f3f94be3362a09521
matoototo <furrowedsip@gmail.com>
2021-07-01 18:07:49 +0200

    Order articles by percentage of unknown words.

---
+++ order_articles.py
@@ -0,0 +1,16 @@
+def percentage_known_order(known, articles):
+    known = frozenset(known)
+    ordered = []
+    for article in articles:
+        article_lemmas = frozenset(article['lemmas'])
+        percentage = len(known.intersection(article_lemmas))/len(article['lemmas'])
+        ordered.append({'url': article['url'], 'percentage': percentage, 'unknown': list(article_lemmas.difference(known))})
+    ordered.sort(key=(lambda x : x['perc']), reverse=True)
+    return ordered
+
+# import json
+
+# known_lemmas = json.load(open('data/extracted-anki.json'))
+# articles = json.load(open('data/articles-lemmified.json'))
+
+# print(percentage_known_order(known_lemmas, articles)[0:1])

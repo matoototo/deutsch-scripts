bd10fba06909db795bc66df3192fd4341eb04f59
mato <31648945+matoototo@users.noreply.github.com>
2021-10-15 10:39:15 +0200

    Add project for C++ version of order_articles.

---
+++ order_articles/order_articles.cpp
@@ -0,0 +1,12 @@
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <rapidjson/istreamwrapper.h>
+#include <iostream>
+#include <fstream>
+
+using namespace rapidjson;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}

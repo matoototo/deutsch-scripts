863525798002309c27bbcd7c82a83df95ed6f732
mato <31648945+matoototo@users.noreply.github.com>
2021-10-16 21:04:04 +0200

    Add DW article support in create_epub.

--- create_epub.py
+++ create_epub.py
@@ -14,19 +14,40 @@ parser.add_argument('-n', action='store_true', help='if present, filters out art
 parser.add_argument('-s', action='store_true', help='if present, filters out articles with the sport tag.')
 parser.add_argument('-k', action='store_true', help='if present, filters out articles with the kultur tag.')
 parser.add_argument('-v', action='store_true', help='if present, filters out articles with the vermischtes tag.')
+parser.add_argument('--src', metavar='source', type=str, help='source of the content in the JSON file, {nl | dw}', required=True)
+
+
+def nl_tag_filter(article):
+    return not any(tag in article['url'] for tag in f_tags)
+
+def get_title():
+    if args.src == 'nl':
+        return ('Nachrichtenleicht ' + date.today().strftime('%d.%m.%Y') + ' (' + char_tag + ')').strip()
+    elif args.src == 'dw':
+        return ('Deutsche Welle ' + date.today().strftime('%d.%m.%Y')).strip()
+
+def get_id(article):
+    if args.src == 'nl':
+        return article['url'][article['url'].find('article_id=')+11:]
+    elif args.src == 'dw':
+        return article['url'][article['url'].find('/a-')+3:]
+
 
 args = parser.parse_args()
 out_filename = args.o
 in_filename = args.i
 limit = args.l
 add_mined = args.m
+source = args.src
 
+# Nachrichtenleicht tags
 filters = [args.n, args.s, args.k, args.v]
 tags = ["2042", "2039", "2045", "2046"]
 tags_char = ['Nachrichten', 'Sport', 'Kultur', 'Vermischtes']
 f_tags = [t for t, f in zip(tags, filters) if f]
 char_tag = " ".join([t for t, f in zip(tags_char, filters) if not f])
-title = ('Nachrichtenleicht ' + date.today().strftime('%d.%m.%Y') + ' (' + char_tag + ')').strip()
+
+title = get_title()
 
 book = epub.EpubBook()
 
@@ -38,8 +59,8 @@ book.set_cover(str(args.cover), open(args.cover, 'rb').read()) if args.cover els
 book.add_author('Deutschlandfunk')
 
 articles = json.load(open(in_filename))
-
-articles = filter(lambda article : not any(tag in article['url'] for tag in f_tags), articles)
+if args.src == 'nl':
+    articles = [article for article in articles if nl_tag_filter(article)]
 
 book.toc = []
 book.spine = []
@@ -47,7 +68,9 @@ book.spine = []
 def remove_img_tag(html):
     start = html.find('<div class=\"image\"')
     end = html.find('</div>')
-    return html[:start] + html[end+6:]
+    if start != -1:
+        return html[:start] + html[end+6:]
+    return html
 
 def insert_mined_words(html, mined):
     mined_tag = "<p style=\"font-size: 0.8em\"> " + ', '.join(mined) + "</p> <hr/>"
@@ -60,7 +83,7 @@ for article in articles:
     start = article['content'].find('<h2>')
     end = article['content'].find('</h2>')
     title = article['content'][start+4:end]
-    id = article['url'][article['url'].find('article_id=')+11:]
+    id = get_id(article)
     article['content'] = remove_img_tag(article['content'])
     if (add_mined): article['content'] = insert_mined_words(article['content'], article['mined'])
     c = epub.EpubHtml(title=title, file_name=f'${id}.xhtml', lang='de')

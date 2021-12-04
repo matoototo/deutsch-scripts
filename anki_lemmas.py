e15e9217ed69b0d125fe267bdbb7956c0d3e68ca
mato <31648945+matoototo@users.noreply.github.com>
2021-12-04 19:34:11 +0100

    Restructure anki_lemmas.py

--- anki_lemmas.py
+++ anki_lemmas.py
@@ -5,36 +5,41 @@ import pathlib
 import re
 from lemmify import lemmatize_sentence
 
-parser = argparse.ArgumentParser(description='Lemmify words from the specified fields in a given Anki deck.')
-parser.add_argument('-i', metavar='filepath', type=pathlib.Path, help='filepath pointing to an anki2 file', required=True)
-parser.add_argument('-o', metavar='filepath', type=pathlib.Path, help='filepath pointing to the output JSON file', required=True)
-parser.add_argument('-f', metavar='fields', type=str, help='fields from which the lemmas will be extracted', nargs='+', required=True)
-args = parser.parse_args()
-in_filename = args.i
-out_filename = args.o
-target_fields = args.f
 
-con = sqlite3.connect(in_filename)
-cur = con.cursor()
+def lemmify_deck(in_filename, target_fields):
+    con = sqlite3.connect(in_filename)
+    cur = con.cursor()
 
-cur.execute('SELECT * FROM col;')
-note_types = json.loads(cur.fetchone()[9])
+    cur.execute('SELECT * FROM col;')
+    note_types = json.loads(cur.fetchone()[9])
 
-cur.execute('SELECT * FROM notes;')
-rows = cur.fetchall()
-rows = list(map(lambda row : {'mid': row[2], 'data': row[6].split('\x1f')}, rows))
+    cur.execute('SELECT * FROM notes;')
+    rows = cur.fetchall()
+    rows = list(map(lambda row : {'mid': row[2], 'data': row[6].split('\x1f')}, rows))
 
-def clean(row):
-    row = re.sub(r"</?\w+>", " ", row)
-    return row
+    def clean(row):
+        row = re.sub(r"</?\w+>", " ", row)
+        return row
 
-extracted = []
-for row in rows:
-    for field in note_types[str(row['mid'])]['flds']:
-        if field['name'] in target_fields:
-            extracted.append(clean(row['data'][field['ord']]))
+    extracted = []
+    for row in rows:
+        for field in note_types[str(row['mid'])]['flds']:
+            if field['name'] in target_fields:
+                extracted.append(clean(row['data'][field['ord']]))
 
-lemmas = list(set(lemmatize_sentence(" ".join(extracted))))
+    return list(set(lemmatize_sentence(" ".join(extracted))))
 
-with open(out_filename, 'w') as outfile:
-    json.dump(lemmas, outfile)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Lemmify words from the specified fields in a given Anki deck.')
+    parser.add_argument('-i', metavar='filepath', type=pathlib.Path, help='filepath pointing to an anki2 file', required=True)
+    parser.add_argument('-o', metavar='filepath', type=pathlib.Path, help='filepath pointing to the output JSON file', required=True)
+    parser.add_argument('-f', metavar='fields', type=str, help='fields from which the lemmas will be extracted', nargs='+', required=True)
+    args = parser.parse_args()
+    in_filename = args.i
+    out_filename = args.o
+    target_fields = args.f
+
+    lemmas = lemmify_deck(in_filename, target_fields)
+    with open(out_filename, 'w') as outfile:
+        json.dump(lemmas, outfile)

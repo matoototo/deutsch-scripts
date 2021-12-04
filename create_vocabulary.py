8310c877fcd75045e63009c71abb47e5119cf547
mato <31648945+matoototo@users.noreply.github.com>
2021-12-04 19:57:08 +0100

    Restructure create_vocabulary.py

--- create_vocabulary.py
+++ create_vocabulary.py
@@ -3,20 +3,6 @@ import argparse
 import pathlib
 from collections import Counter
 
-parser = argparse.ArgumentParser(description='Creates a list of lemmas used in a given (articles) JSON,\
-                                              counting their frequency and filtering out those that are likely bad.')
-parser.add_argument('-i', metavar='filepath', type=pathlib.Path, help='filepath pointing to a JSON file', required=True)
-parser.add_argument('-o', metavar='filepath', type=pathlib.Path, help='filepath pointing to the output JSON file', required=True)
-
-args = parser.parse_args()
-in_filename = args.i
-out_filename = args.o
-
-articles = json.load(open(in_filename))
-
-lemmas = []
-for article in articles:
-    lemmas += sum([x for x in article['sentence-lemmas']], [])
 
 def bad_char_predicate(word):
     bad = ['<', '\n', '>', '\"', '’', '.', ':', ',', '-', '!', '?', *list('0123456789'), '\u00a0']
@@ -25,7 +11,30 @@ def bad_char_predicate(word):
             return False
     return True
 
-counted = Counter(lemmas)
-filtered = list(filter(lambda x:  bad_char_predicate(x), set(lemmas)))
-glued = {k : v for k, v in dict(counted.most_common(None)).items() if k in filtered}
-json.dump(glued, open(out_filename, 'w'), separators=(',', ': '), indent=4)
+
+def create_vocab(in_filename):
+    articles = json.load(open(in_filename))
+    lemmas = []
+
+    for article in articles:
+        lemmas += sum([x for x in article['sentence-lemmas']], [])
+
+    counted = Counter(lemmas)
+    filtered = list(filter(lambda x:  bad_char_predicate(x), set(lemmas)))
+    glued = {k : v for k, v in dict(counted.most_common(None)).items() if k in filtered}
+
+    return glued
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Creates a list of lemmas used in a given (articles) JSON,\
+                                                counting their frequency and filtering out those that are likely bad.')
+    parser.add_argument('-i', metavar='filepath', type=pathlib.Path, help='filepath pointing to a JSON file', required=True)
+    parser.add_argument('-o', metavar='filepath', type=pathlib.Path, help='filepath pointing to the output JSON file', required=True)
+
+    args = parser.parse_args()
+    in_filename = args.i
+    out_filename = args.o
+
+    glued = create_vocab(in_filename)
+    json.dump(glued, open(out_filename, 'w'), separators=(',', ': '), indent=4)

b7ccc3cc23b2aab7fd27606d8a4f1150ca8caabb
mato <31648945+matoototo@users.noreply.github.com>
2022-02-08 21:34:51 +0100

    Ignore automatically generated transcripts in scraper.

--- scrapers/get_transcripts.py
+++ scrapers/get_transcripts.py
@@ -4,7 +4,7 @@ import pathlib
 from pyyoutube import Api
 from youtube_transcript_api import YouTubeTranscriptApi
 
-parser = argparse.ArgumentParser(description='Gets transcripts of every video from a given channel name.')
+parser = argparse.ArgumentParser(description='Gets manual German transcripts of every video from a given channel name.')
 parser.add_argument('-i', metavar='id', type=str, help='channel name', required=True)
 parser.add_argument('-o', metavar='filepath', type=pathlib.Path, help='filepath pointing to the output JSON file', required=True)
 parser.add_argument('-k', metavar='api_key', type=str, help='google API key', required=True)
@@ -21,8 +21,15 @@ playlist_id = channel_res.items[0].contentDetails.relatedPlaylists.uploads
 playlist_res = api.get_playlist_items(playlist_id=playlist_id, count = None)
 
 videos = []
-for video in playlist_res.items:
-    transcript = YouTubeTranscriptApi.get_transcript(video.contentDetails.videoId, languages=('de', ))
+for i, video in enumerate(playlist_res.items):
+    if i%10 == 0:
+        print(i, '/', len(playlist_res.items))
+
+    try:
+        transcript = YouTubeTranscriptApi.list_transcripts(video.contentDetails.videoId).find_manually_created_transcript(['de']).fetch()
+    except:
+        continue # No manual transcript found
+
     transcript = ' '.join(list(map(lambda x : x['text'].replace('\n', ' '), transcript)))
     videos.append({'title': video.snippet.title, 'id': video.contentDetails.videoId,
                   'transcript': transcript})
